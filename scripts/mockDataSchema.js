ce13b8172e4504f71a37ebeed6153bf04f48d05a
Glitch (broad-spectrum-windshield) <none>
2023-04-16 13:07:28 +0530

    fix(update)

--- scripts/mockDataSchema.js
+++ scripts/mockDataSchema.js
@@ -1,4 +1,4 @@
-let users = {
+let usersSchema = {
   type: 'object',
   properties: {
     users: {
@@ -33,5 +33,35 @@ let users = {
   required: ['users'],
 }
 
+const productSchema = {
+  title: 'Product',
+  description: "A product from Acme's catalog",
+  type: 'object',
+  properties: {
+    id: {
+      description: 'The unique identifier for a product',
+      type: 'integer',
+    },
+    name: {
+      description: 'Name of the product',
+      type: 'string',
+    },
+    price: {
+      type: 'number',
+      minimum: 0,
+      exclusiveMinimum: true,
+    },
+    tags: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+      minItems: 1,
+      uniqueItems: true,
+    },
+  },
+  required: ['id', 'name', 'price'],
+}
+
 // module.exports = usersSchema
-module.exports = { users }
+module.exports = { users: usersSchema, product: productSchema }
