87d4fd635a5b34a65c4790549f80e07a66ef8e0e
Glitch (broad-spectrum-windshield) <none>
2023-04-16 15:18:52 +0530

    feat(mock-server): file based routes.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 
-// const router = jsonServer.router('src/dynamic/db.json')
-const router = jsonServer.router('src/db.json')
+// const router = jsonServer.router('src/seeds/db.json')
+// const router = jsonServer.router('src/db.json')
+const router = jsonServer.router(require('./src/db')())
 
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3500
+const port = process.env.PORT || 8000
 
 console.log('Server running on PORT', port)
 server.use(middlewares)
---
+++ src/db.js
@@ -0,0 +1,7 @@
+const aggregate = require('./aggregate.json')
+const timeseries = require('./timeseries.json')
+
+module.exports = () => ({
+  aggregate,
+  timeseries,
+})
