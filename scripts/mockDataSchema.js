829e48db4be3bd331397670596e8d722f306f33b
Glitch (broad-spectrum-windshield) <none>
2023-04-16 13:53:39 +0530

    fix(update)

--- scripts/mockDataSchema.js
+++ scripts/mockDataSchema.js
@@ -1,38 +1,3 @@
-let usersSchema = {
-  type: 'object',
-  properties: {
-    users: {
-      type: 'array',
-      minItems: 3,
-      maxItems: 10,
-      items: {
-        type: 'object',
-        properties: {
-          id: {
-            type: 'number',
-            unique: true,
-            minimum: 1,
-          },
-          firstName: {
-            type: 'string',
-            faker: 'name.firstName',
-          },
-          lastName: {
-            type: 'string',
-            faker: 'name.lastName',
-          },
-          email: {
-            type: 'string',
-            faker: 'internet.email',
-          },
-        },
-        required: ['id', 'type', 'lastname', 'email'],
-      },
-    },
-  },
-  required: ['users'],
-}
-
 const productSchema = {
   title: 'Product',
   description: "A product from Acme's catalog",
@@ -63,5 +28,43 @@ const productSchema = {
   required: ['id', 'name', 'price'],
 }
 
+const usersSchema = {
+  type: 'array',
+  minItems: 3,
+  maxItems: 10,
+  items: {
+    type: 'object',
+    properties: {
+      id: {
+        type: 'number',
+        unique: true,
+        minimum: 1,
+      },
+      firstName: {
+        type: 'string',
+        faker: 'name.firstName',
+      },
+      lastName: {
+        type: 'string',
+        faker: 'name.lastName',
+      },
+      email: {
+        type: 'string',
+        faker: 'internet.email',
+      },
+    },
+    required: ['id', 'type', 'lastname', 'email'],
+  },
+}
+
+let results = {
+  type: 'object',
+  properties: {
+    users: usersSchema,
+    products: productSchema,
+  },
+  required: ['users', 'products'],
+}
+
 // module.exports = usersSchema
-module.exports = { users: usersSchema, product: productSchema }
+module.exports = { results: results }
